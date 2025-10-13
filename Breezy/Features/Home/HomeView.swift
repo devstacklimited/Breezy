30766c8e4bb5b465e19c43b91887e243cc6819a6
Usama Javed <xcoderpc@gmail.com>
2025-10-13 05:14:42 +0500

    Designed SplashView and implemented animation

---
+++ Breezy/Features/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Breezy
+//
+//  Created by Mian Usama on 13/10/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        Text("🌤 Home Screen")
+            .font(.title)
+            .bold()
+            .foregroundColor(.blue)
+    }
+}
