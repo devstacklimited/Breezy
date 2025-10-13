aba5486cd0386362b75122033d17d236dfcfe453
Usama Javed <xcoderpc@gmail.com>
2025-10-13 05:02:09 +0500

    Project setup

---
+++ Breezy/BreezyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezyApp.swift
+//  Breezy
+//
+//  Created by Mian Usama on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreezyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
